18b5dca517cf99b814edaa5224739910d8197b6a
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-03 11:04:21 -0500

    Implement Google authentication and create profile page

---
+++ src/app/components/LogInGoogle.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Image from "next/image";
+
+export default function LogInGoogle() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      router.push('/profile');
+    }
+  }, [status, router]);
+
+  const handleSignIn = async () => {
+    const response = await signIn('google');
+    console.log(response);
+  };
+
+  return (
+    <>
+      <button
+        type='button'
+        onClick={handleSignIn}
+        className='w-full flex items-center justify-center gap-2 border px-12 py-3 rounded-md border-gray-200 cursor-pointer hover:bg-blue-50'>
+        <Image src='/google.svg' alt='google' width={10} height={10} className='w-6' />
+        <span> Continue with Google</span>
+      </button>
+    </>
+  );
+}
