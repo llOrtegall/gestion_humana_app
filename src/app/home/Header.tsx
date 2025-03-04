6e24afe6a9a1a081745c5df209d30a6a42fa9f58
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-04 13:15:33 -0500

    next config images adds components

--- src/app/home/Header.tsx
+++ src/app/home/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
 
   return (
     <>
-      <figure className='flex items-center justify-center px-4 py-2 bg-gray-100 border-b border-gray-200'>
+      <figure className='flex items-center justify-center px-4 py-3 bg-gray-100 border-b border-gray-200'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
         </svg>
--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,18 +1,24 @@
-import Link from 'next/link';
 import Header from './Header';
+import { auth } from '@/auth';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+
+  const user = session?.user;
+
   return (
     <section className='flex h-screen'>
 
-      <header className='w-2/12 border-r border-gray-200'>
+      <header className='w-3xs border-r border-gray-200'>
         <Header />
       </header>
 
-      <section className='w-10/12'>
-        <aside className="bg-gray-100 px-4 py-2 border-b border-gray-200">
-          <nav>
-            <ul className="container mx-auto flex space-x-4">
+      <section className='w-auto flex-1'>
+        <aside className="flex items-center justify-between bg-gray-100 px-4 border-b border-gray-200">
+          <nav className='py-3'>
+            <ul className="container mx-auto items-center justify-center flex space-x-4">
               <li>
                 <Link href="/home" className="flex text-gray-400">Home</Link>
               </li>
@@ -21,6 +27,25 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               </li>
             </ul>
           </nav>
+
+          <button className='cursor-pointer'>
+            {
+              user?.image ? (
+                <Image
+                  className='rounded-full hover:opacity-80'
+                  src={user.image}
+                  alt='profile'
+                  width={30}
+                  height={30}
+                />
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+              )
+            }
+          </button>
+
         </aside>
         {children}
       </section>
