d4541b7eea37fa4df17b51387eb3868c5daddb7d
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-04 13:33:11 -0500

    Update image configuration, refactor layout, and add UserButton component

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,12 +1,8 @@
+import ButtonUser from '@/app/components/UserBotton';
 import Header from './Header';
-import { auth } from '@/auth';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  const session = await auth();
-
-  const user = session?.user;
 
   return (
     <section className='flex h-screen'>
@@ -28,23 +24,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
             </ul>
           </nav>
 
-          <button className='cursor-pointer'>
-            {
-              user?.image ? (
-                <Image
-                  className='rounded-full hover:opacity-80'
-                  src={user.image}
-                  alt='profile'
-                  width={30}
-                  height={30}
-                />
-              ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </svg>
-              )
-            }
-          </button>
+          <ButtonUser />
 
         </aside>
         {children}
