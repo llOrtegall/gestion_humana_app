58b428690fb41898339d4680d5c67fee4f4eb1da
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-06 10:52:14 -0500

    Enhance HomePage layout with navigation menu and update styling

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,34 @@
 export default function HomePage() {
   return (
-    <section className="">
-      test
+    <section>
+      <ul className="bg-amber-50 flex border-b border-gray-200 text-gray-600">
+        <li>
+          <span
+            className="px-6 py-4 flex border-r border-gray-200"
+          >
+            Inicio
+          </span>
+        </li>
+        <li>
+          <a
+            className="px-6 py-4 flex"
+            href="/login"
+          >Vista General</a>
+        </li>
+        <li>
+          <a
+            className="px-6 py-4 flex"
+            href="/register"
+          >Objetivos</a>
+        </li>
+        <li>
+          <a
+            className="px-6 py-4 flex"
+            href="/register"
+          >Reuniones</a>
+        </li>
+      </ul>
+      dashboard
     </section>
   )
 }
