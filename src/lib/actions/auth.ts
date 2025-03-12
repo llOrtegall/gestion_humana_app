65a89cd874c4eba0f26b6a21157447e0a51e23d6
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-12 14:33:03 -0500

    Add signup function with form validation using Zod schema

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { signIn, signOut } from '@/auth';
+import { FormState, SignupFormSchema } from '../definitions';
 
 export const LoginGoogle = async () => {
   await signIn('google', { redirectTo: '/home/inicio/vistageneral' });
@@ -8,4 +9,19 @@ export const LoginGoogle = async () => {
 
 export const LogoutGoogle = async () => {
   await signOut({ redirectTo: '/' });
+}
+
+export async function sigup(state: FormState, formData: FormData) {
+  // validate form fields
+  const validatedFields = SignupFormSchema.safeParse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  if (!validatedFields.success) {
+    return { errors: validatedFields.error.flatten().fieldErrors }
+  }
+
+  console.log(validatedFields.data);
+
 }
